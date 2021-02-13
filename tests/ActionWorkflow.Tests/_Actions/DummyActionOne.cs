59cb2499d88eb350eb38f24fff495dbe306cf1db
Ieeax <dev@leeax.de>
2021-02-13 22:58:51 +0100

    Added tests for the builder-class

---
+++ tests/ActionWorkflow.Tests/_Actions/DummyActionOne.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+
+namespace ActionWorkflow.Tests
+{
+    public class DummyActionOne : IAction<string>
+    {
+        public Task ExecuteAsync(string context)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ tests/ActionWorkflow.Tests/_Actions/DummyActionTwo.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+
+namespace ActionWorkflow.Tests
+{
+    public class DummyActionTwo : IAction<string>
+    {
+        public Task ExecuteAsync(string context)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
